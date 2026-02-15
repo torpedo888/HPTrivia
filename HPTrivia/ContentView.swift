3184b1240e1317a53ece5dbc74ea851d00993c33
Batorligeti Ferenc (Contractor) <ferenc.batorligeti@capgemini.com>
2026-02-15 19:50:15 +0000

    Initial Commit

---
+++ HPTrivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HPTrivia
+//
+//  Created by Ferenc Batorligeti on 2026. 02. 15..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Ferenc Batorligeti on 2026. 02. 15..
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
