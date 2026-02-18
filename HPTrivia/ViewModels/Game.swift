22b4b3f781a63b0e05ddb53cca13412582805219
Batorligeti Ferenc (Contractor) <ferenc.batorligeti@capgemini.com>
2026-02-18 23:33:06 +0000

    game logic questions and scores

--- HPTrivia/ViewModels/Game.swift
+++ HPTrivia/ViewModels/Game.swift
@@ -9,4 +9,49 @@ import SwiftUI
 @Observable
 class Game {
     var bookQuestions = BookQuestions()
+
+    var gameScore = 0
+
+    var questionScore = 5
+    var recentScores = [0, 0, 0]
+
+    var activeQuestions: [Question] = []
+    var answeredQuestions: [Int] = []
+    var currentQuestion = try! JSONDecoder().decode(Question.self, from:
+            Data(
+                contentsOf: Bundle.main.url(
+                    forResource: "trivia",
+                    withExtension: "json")!))[0]
+    var answers: [String] = []
+
+    func StartGame() {
+       for book in bookQuestions.books {
+           if book.status == "active" {
+               for question in book.questions {
+                   activeQuestions.append(question)
+               }
+           }
+        }
+
+        newQuestion()
+    }
+
+    func newQuestion() {
+
+    }
+
+    func correct() {
+
+    }
+
+    func endGame() {
+        recentScores[2] = recentScores[1]
+        recentScores[1] = recentScores[0]
+        recentScores[0] = gameScore
+
+        gameScore = 0
+        activeQuestions = []
+        answeredQuestions = []
+    }
+
 }
